592643af4052a629a7de5c02217ef0784bc50267
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-07-25 10:47:55 +0300

    Added logs

--- Task6/task6.js
+++ Task6/task6.js
@@ -1,12 +1,12 @@
-export function nNodeTreeSearchRecursion (tree, node) {
-  if (tree.node === node) return tree;
+function nNodeTreeSearchRecursion (tree, value) {
+  if (tree.node === value) return tree;
   for (const child of tree.children) {
-    const obj = nNodeTreeSearchRecursion(child, node)
+    const obj = nNodeTreeSearchRecursion(child, value)
     if (obj) return obj;
   }
 }
 
-export function nNodeTreeSearchNotRecursion(tree, value) {
+function nNodeTreeSearchNotRecursion(tree, value) {
 
   if (tree.node === value) return tree
   const array = [...tree.children]
@@ -26,3 +26,68 @@ export function nNodeTreeSearchNotRecursion(tree, value) {
     if (node) return node;
   }
 }
+
+const tree = {
+  node: 1,
+  children: [
+    {
+      node: 2,
+      children: [
+        {
+          node: 3,
+          children: [
+            {
+              node: 4,
+              children: [
+                {
+                  node: 5,
+                  children: [],
+                },
+                {
+                  node: 6,
+                  children: [],
+                },
+                {
+                  node: 7,
+                  children: [],
+                },
+                {
+                  node: 8,
+                  children: [],
+                },
+              ],
+            },
+            {
+              node: 9,
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      node: 10,
+      children: [
+        {
+          node: 11,
+          children: [
+            {
+              node: 12,
+              children: [],
+            },
+          ],
+        },
+        {
+          node: 13,
+          children: [],
+        },
+        {
+          node: 14,
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+console.log(nNodeTreeSearchRecursion(tree, 11));
+console.log(nNodeTreeSearchNotRecursion(tree, 11));
