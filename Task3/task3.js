f80f11cc866136ae22e648cf47156105dd22d341
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-07-27 08:13:44 +0300

    Changed function

--- Task3/task3.js
+++ Task3/task3.js
@@ -1,16 +1,18 @@
 function pathGen (path, value, obj) {
-  let object = {...obj};
 
   path.reduce((prev, current, currentIndex) => {
     if (currentIndex === path.length - 1) {
       prev[current] = value
-      return object;
+      return obj;
+    }
+    if (prev[current]) {
+      return prev[current];
     }
     prev[current] = {};
     return prev[current];
-  }, object )
+  }, obj )
 
-  return object;
+  return obj;
 }
 
-console.log(pathGen(['user', 'name'], 'Sam', {}))
+console.log(pathGen(['user', 'name'], 'Sam', { user: {city: 'Chicago'} }))
